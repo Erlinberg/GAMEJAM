a2c30b4aa6899f4293b4a533a74934eb9ff2186e
Erlinberg <develierol@gmail.com>
2018-10-15 23:38:09 +0300

    с

--- xxx/Assets/Bin.cs
+++ xxx/Assets/Bin.cs
@@ -6,15 +6,14 @@ public class Bin : MonoBehaviour {
 
     public int KilledBug = 0;
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-
-        if (collision.transform.gameObject.name == "bug" & collision.transform.GetComponent<bug>().died == true)
+        if (collision.tag == "bug" & collision.GetComponent<bug>().died == true)
         {
             if (Input.GetMouseButtonUp(0))
             {
                 KilledBug += 1;
-                Destroy(collision.transform.gameObject);
+                Destroy(collision.gameObject);
             }
         }
     }
